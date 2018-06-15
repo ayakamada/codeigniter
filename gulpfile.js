856c17904175819e15f4d2e34f73efc86b779aea
ayakamada <sdkamada@gmail.com>
2018-06-15 15:20:20 +0900

    sass環境準備

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+/******************
+ packages
+****************/
+var
+  gulp = require('gulp'),
+  sass = require('gulp-sass');
+
+/******************
+directlies
+******************/
+var
+  SCSS_FILE = "./my-site/public/assets/sass/**/*.scss",
+  CSS_FILE = "./my-site/public/assets/css/**/*.css",
+  JS_FILE = "./my-site/public/assets/js/**/*.js";
+
+
+/******************
+sass compile
+******************/
